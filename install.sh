5b03fef0aaca4013b9f42c52b90ff00dfe534bf6
Fernando Motolese <81449156+motolese@users.noreply.github.com>
2022-03-03 21:49:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
 
+cp /home/admin/scripts/MyNodeBTC-Install/etc/nginx/sites-enabled/https_btcpayserver-alt.conf /etc/nginx/sites-enabled/https_btcpayserver-alt.conf
+cp /home/admin/scripts/MyNodeBTC-Install/etc/nginx/sites-enabled/https_btcrpcexplorer-alt.conf /etc/nginx/sites-enabled/https_btcrpcexplorer-alt.conf
+cp /home/admin/scripts/MyNodeBTC-Install/etc/nginx/sites-enabled/https_lnbits-alt.conf /etc/nginx/sites-enabled/https_lnbits-alt.conf
+cp /home/admin/scripts/MyNodeBTC-Install/etc/nginx/sites-enabled/https_lndhub-alt.conf /etc/nginx/sites-enabled/https_lndhub-alt.conf
+cp /home/admin/scripts/MyNodeBTC-Install/etc/nginx/sites-enabled/https_mempoolspace-alt.conf /etc/nginx/sites-enabled/https_lndhub-alt.conf
